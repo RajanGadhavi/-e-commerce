c5139889fc4dfdf829c5a280f289ae463e18aebe
RajanGadhavi <rajangadhavi5@gmail.com>
2025-02-18 17:29:57 +0530

    getting started

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HomeComponent]
 })
 export class AppComponent {
   title = 'e-commerce';
