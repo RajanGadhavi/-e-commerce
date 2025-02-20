c0223814e2247d2967ca9bdee44c164f65fb0ddb
RajanGadhavi <rajangadhavi5@gmail.com>
2025-02-20 17:11:28 +0530

    Some improvements

--- e-commerce/src/app/app.component.ts
+++ e-commerce/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomeComponent, NavBarComponent]
+    imports: [RouterOutlet, HomeComponent, NavBarComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'e-commerce';
